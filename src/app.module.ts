ee3585da814c59dae4221ff9362f488469f54ff8
Mu-Aleem <se.aleemkhalid786@gmail.com>
2025-09-23 11:39:22 +0500

    add the twilio

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { CloudinaryConfigService } from './cloudinary/cloudinary.provider';
 import { CardsModule } from './cards/cards.module';
 import { StripeModule } from './stripe/stripe.module';
 import { RoundUpModule } from './round-up/round-up.module';
+import { TwilioModule } from './twilio/twilio.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { RoundUpModule } from './round-up/round-up.module';
     CardsModule,
     StripeModule,
     RoundUpModule,
+    TwilioModule,
   ],
   controllers: [AppController],
   providers: [AppService],
