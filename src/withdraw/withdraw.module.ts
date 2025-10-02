5944d0907fb57aa2c7262fab055c228e40ae78a6
Mu-Aleem <se.aleemkhalid786@gmail.com>
2025-10-02 17:14:23 +0500

    fix the erros

--- src/withdraw/withdraw.module.ts
+++ src/withdraw/withdraw.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { WithdrawService } from './withdraw.service';
 import { WithdrawController } from './withdraw.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { StripeService } from 'src/stripe/stripe.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [WithdrawController],
-  providers: [WithdrawService],
+  providers: [WithdrawService, StripeService],
+  exports: [WithdrawService],
 })
 export class WithdrawModule {}
